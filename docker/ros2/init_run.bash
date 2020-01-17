4427a2a9521131a988fe91b17f5b1a69cc39b064
bonohub13 <ken.saito.0813@gmail.com>
2020-01-17 21:51:23 +0900

    added modes for docker and other environments

--- docker/ros2/init_run.bash
+++ docker/ros2/init_run.bash
@@ -3,18 +3,34 @@
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
 
-apt update
-apt install -y ros-dashing-ros-base
+if [$HOME = "/root"]; then
+	apt update
+	apt install -y ros-dashing-ros-base
+else
+	sudo apt update
+	sudo apt install -y \
+		ros-dashing-ros-base
+fi
 
 echo "source /opt/ros/dashing/setup.bash >> ~/.bashrc"
 source ~/.bashrc
 
-apt install -y \
-	python3-argcomplete \
-	python3-colcon-common-extensions \
-	python3-rosdep
+if [$HOME = "/root"]; then
+	apt install -y \
+		python3-argcomplete \
+		python3-colcon-common-extensions \
+		python3-rosdep
 
-rosdep init
-rosdep update
+	rosdep init
+	rosdep update
+else
+	sudo apt install -y \
+		python3-argcomplete \
+		python3-colcon-common-extensions \
+		python3-rosdep
+
+	sudo rosdep init
+	sudo rosdep update
+fi
 
 echo "Finished ros2 installation sequence!"
