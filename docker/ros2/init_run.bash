e5936e0f5ceb0ec2e964e465ffe97438d7cfe6c9
bonohub13 <ken.saito.0813@gmail.com>
2020-01-21 16:57:30 +0900

    updated installer

--- docker/ros2/init_run.bash
+++ docker/ros2/init_run.bash
@@ -1,37 +1,20 @@
 #!/usr/bin/env bash
 
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
 
-if [$HOME = "/root"]; then
-	sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-	apt update
-	apt install -y ros-dashing-ros-base
-else
-	sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-	sudo apt update
-	sudo apt install -y \
-		ros-dashing-ros-base
-fi
+apt update
+apt install -y ros-dashing-ros-base
 
 echo "source /opt/ros/dashing/setup.bash >> ~/.bashrc"
 source ~/.bashrc
 
-if [$HOME = "/root"]; then
-	apt install -y \
-		python3-argcomplete \
-		python3-colcon-common-extensions \
-		python3-rosdep
+apt install -y \
+	python3-argcomplete \
+	python3-colcon-common-extensions \
+	python3-rosdep
 
-	rosdep init
-	rosdep update
-else
-	sudo apt install -y \
-		python3-argcomplete \
-		python3-colcon-common-extensions \
-		python3-rosdep
-
-	sudo rosdep init
-	sudo rosdep update
-fi
+rosdep init
+rosdep update
 
 echo "Finished ros2 installation sequence!"
